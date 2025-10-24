df078358fb805cedd75a213ae3e471ae1b0e9048
makaveli10 <vineet.suryan@collabora.com>
2025-10-24 11:59:57 +0200

    lora: Add Instruction Finetuning support
    - Add masked loss computation on assistant responses only
    - Implement Vulkan masked cross-entropy loss shader & count_equal shader
    - Support default ChatML template & custom jinja chat templates
    
    Rebase on b8828:
    * Drop .get() calls for context access.
    * Update op count.
    * Shader includes are now .glsl.
    * Don't add cross entropy loss back shader to Vulkan backend yet.
    
    Co-authored-by: Lubosz Sarnecki <lubosz.sarnecki@collabora.com>

--- ggml/src/ggml-cpu/ggml-cpu.c
+++ ggml/src/ggml-cpu/ggml-cpu.c
@@ -1808,6 +1808,10 @@ static void ggml_compute_forward(struct ggml_compute_params * params, struct ggm
             {
                 ggml_compute_forward_count_equal(params, tensor);
             } break;
+        case GGML_OP_COUNT_EQUAL_MASKED:
+            {
+                ggml_compute_forward_count_equal_masked(params, tensor);
+            } break;
         case GGML_OP_REPEAT:
             {
                 ggml_compute_forward_repeat(params, tensor);
@@ -2097,6 +2101,16 @@ static void ggml_compute_forward(struct ggml_compute_params * params, struct ggm
                 ggml_compute_forward_cross_entropy_loss_back(params, tensor);
             }
             break;
+        case GGML_OP_CROSS_ENTROPY_LOSS_MASKED:
+            {
+                ggml_compute_forward_cross_entropy_loss_masked(params, tensor);
+            }
+            break;
+        case GGML_OP_CROSS_ENTROPY_LOSS_MASKED_BACK:
+            {
+                ggml_compute_forward_cross_entropy_loss_masked_back(params, tensor);
+            }
+            break;
         case GGML_OP_OPT_STEP_ADAMW:
             {
                 ggml_compute_forward_opt_step_adamw(params, tensor);
@@ -2247,6 +2261,7 @@ static int ggml_get_n_tasks(struct ggml_tensor * node, int n_threads) {
         case GGML_OP_COUNT_EQUAL:
         case GGML_OP_SOLVE_TRI:
         case GGML_OP_GATED_DELTA_NET:
+        case GGML_OP_COUNT_EQUAL_MASKED:
             {
                 n_tasks = n_threads;
             } break;
@@ -2444,6 +2459,8 @@ static int ggml_get_n_tasks(struct ggml_tensor * node, int n_threads) {
             } break;
         case GGML_OP_CROSS_ENTROPY_LOSS:
         case GGML_OP_CROSS_ENTROPY_LOSS_BACK:
+        case GGML_OP_CROSS_ENTROPY_LOSS_MASKED:
+        case GGML_OP_CROSS_ENTROPY_LOSS_MASKED_BACK:
         case GGML_OP_OPT_STEP_ADAMW:
         case GGML_OP_OPT_STEP_SGD:
             {
@@ -2828,6 +2845,7 @@ struct ggml_cplan ggml_graph_plan(
                         }
                     } break;
                 case GGML_OP_COUNT_EQUAL:
+                case GGML_OP_COUNT_EQUAL_MASKED:
                     {
                         cur = ggml_type_size(node->type)*n_tasks;
                     } break;
@@ -2966,6 +2984,11 @@ struct ggml_cplan ggml_graph_plan(
                     {
                         const int64_t S_v = node->src[2]->ne[0];
                         cur = S_v * sizeof(float) * n_tasks;
+                    } break;
+                case GGML_OP_CROSS_ENTROPY_LOSS_MASKED:
+                case GGML_OP_CROSS_ENTROPY_LOSS_MASKED_BACK:
+                    {
+                        cur = ggml_type_size(node->type)*(n_tasks + node->src[0]->ne[0]*n_tasks) + sizeof(int64_t)*n_tasks;
                     } break;
                 case GGML_OP_COUNT:
                     {
