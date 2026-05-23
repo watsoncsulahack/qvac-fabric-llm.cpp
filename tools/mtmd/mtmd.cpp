ogad-tether/qvac-ext-lib-llama.cpp

implement logging redirection

tools/mtmd/mtmd.cpp
@@ -359,6 +359,10 @@ void mtmd_free(mtmd_context * ctx) {
     }
 }
 
+void mtmd_log_set_llama_callback(ggml_log_callback llama_cb, void * llama_user_data) {
+    clip_log_use_llama_callback(llama_cb, llama_user_data);
+}
+
 struct mtmd_tokenizer {
     mtmd_context * ctx;
     std::vector<const mtmd_bitmap *> bitmaps;

watsoncsulahack/qvac-fabric-llm.cpp

Co-authored-by: Jesús <jesus.mb.1995@gmail.com>

tools/mtmd/mtmd.cpp
@@ -565,6 +565,10 @@ void mtmd_free(mtmd_context * ctx) {
     delete ctx;
 }
 
+void mtmd_log_set_llama_callback(ggml_log_callback llama_cb, void * llama_user_data) {
+    clip_log_set_callback(llama_cb, llama_user_data);
+}
+
 struct mtmd_tokenizer {
     mtmd_context * ctx;
     std::vector<const mtmd_bitmap *> bitmaps;