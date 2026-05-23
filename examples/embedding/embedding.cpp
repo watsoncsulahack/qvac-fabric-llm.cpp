tetherto/qvac-ext-lib-llama.cpp

[aux] Memory example (embedding)
Adapt embedding example to showcase how to load from memory. Can be configured through environment variables.

examples/embedding/embedding.cpp
@@ -1,14 +1,22 @@
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <thread>
+#include <vector>
+
 #include "arg.h"
 #include "common.h"
+#include "llama-cpp.h"
 #include "log.h"
-#include "llama.h"
-
-#include <ctime>
-#include <algorithm>
 
 #if defined(_MSC_VER)
 #pragma warning(disable: 4244 4267) // possible loss of data
 #endif
+
+#include "load_into_memory.h"
 
 static std::vector<std::string> split_lines(const std::string & s, const std::string & separator = "\n") {
     std::vector<std::string> lines;
@@ -94,7 +102,15 @@ int main(int argc, char ** argv) {
     llama_numa_init(params.numa);
 
     // load the model
-    common_init_result llama_init = common_init_from_params(params);
+    common_init_result llama_init;
+    if (memory_configuration_env_is_set()) {
+        llama_model_params mparams = common_model_params_to_llama(params);
+        common_init_result iparams;
+        llama_model *      model = load_model_from_memory_configuration(params.model.path.c_str(), mparams);
+        llama_init               = common_init_from_model_and_params(model, std::move(iparams), params);
+    } else {
+        llama_init = common_init_from_params(params);
+    }
 
     llama_model * model = llama_init.model.get();
     llama_context * ctx = llama_init.context.get();

watsoncsulahack/qvac-fabric-llm.cpp

Co-authored-by: Lubosz Sarnecki <lubosz.sarnecki@collabora.com>

examples/embedding/embedding.cpp
@@ -1,5 +1,15 @@
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <thread>
+#include <vector>
+
 #include "arg.h"
 #include "common.h"
+#include "llama-cpp.h"
 #include "log.h"
 #include "llama.h"
 
@@ -9,6 +19,10 @@
 
 #if defined(_MSC_VER)
 #pragma warning(disable: 4244 4267) // possible loss of data
+#endif
+
+#ifdef LLAMA_COMMON_TEST_HEADERS
+#include "load_into_memory.h"
 #endif
 
 static std::vector<std::string> split_lines(const std::string & s, const std::string & separator = "\n") {
@@ -134,7 +148,18 @@ int main(int argc, char ** argv) {
     llama_numa_init(params.numa);
 
     // load the model
+#ifdef LLAMA_COMMON_TEST_HEADERS
+    common_init_result_ptr llama_init;
+    if (memory_configuration_env_is_set()) {
+        llama_model_params mparams = common_model_params_to_llama(params);
+        llama_model * model = load_model_from_memory_configuration(params.model.path.c_str(), mparams);
+        llama_init = common_init_from_model_and_params(model, std::move(llama_init), params);
+    } else {
+        llama_init = common_init_from_params(params);
+    }
+#else
     auto llama_init = common_init_from_params(params);
+#endif
 
     auto * model = llama_init->model();
     auto * ctx = llama_init->context();